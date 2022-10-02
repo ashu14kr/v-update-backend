898187181dc2f9c260951d94da7e1101c1d5953f
Ashutosh kumar <itsme@Ashutoshs-MacBook-Air.local>
2022-10-02 13:48:04 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const app = express();
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
